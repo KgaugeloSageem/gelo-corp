925f0107b86fe5a7d2e9684dee9104de46930fc0
Kgaugelo Ramaramela <kgaugelo.ramaramela@standardbank.co.za>
2023-07-27 22:17:44 +0200

    Initial Spring Boot project

---
+++ src/test/java/com/gelocorp/gelomicroservice/GeloMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelocorp.gelomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeloMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
